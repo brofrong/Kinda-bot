0a9efbc439728276401f19e4da07206496b24a22
Dmitri Gorshkov <dga@imtechnologies.ru>
2021-07-21 00:31:51 +0300

    change token

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-export const TOKEN = 'ODM2NzAxMzIzMzkwNDg0NTYw.YIh01A.gaRbgjzWBEx-48OFQF3PIeMshsA';
+export const TOKEN = '';// enter Token
 export const BOT_CHANEL = '837007949556744232';
 export const PREFIX = '/';
